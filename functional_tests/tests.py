31f880af88fc8fb792debe6d6d73318abc801acb
unknown <rileyj@D05915.Boardwalk.corp>
2015-07-02 09:35:02 -0500

    Updated functional test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -75,7 +75,7 @@ class NewVistitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
 
         # User 2 gets a new home page with no items from User 1
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
