684494122c14b39f963689d6c23a94111654412c
Sailor972 <javajeff972@gmail.com>
2015-07-03 16:51:53 -0500

    Updated functional tests.

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -1,11 +1,17 @@
 from .base import FunctionalTest
+from unittest import skip
 
 
 class LayoutAndStylingTest(FunctionalTest):
+    @skip
     def test_layout_and_styling(self):
+        # User goes to the home page
         self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
+
+        # User should notice the input box is centered on the page
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(inputbox.location['x'] +
-                               inputbox.size['width'] / 2, 512, delta=5
-                               )
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5)
--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -6,4 +6,27 @@ class ItemValidationTest(FunctionalTest):
     @skip
     def test_cannot_add_empty_list_items(self):
         # User should not be able to accidentally add an empty list item
-        self.fail('write me!')
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+
+        # The page should refresh and display an error message saying
+        # that list items cannot be blank
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
+
+        # User should be able to try again and enter text for the item
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+
+        # User again accidentally enter a blank item
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+
+        # User should get another error message
+        self.check_for_row_in_list_table('1: Buy milk')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
+
+        # User can once again correct the error by entering text for the item
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Make tea')
