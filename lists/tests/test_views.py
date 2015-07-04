c33c8dded50955eb9a89f72800a2e7baa4275e63
Sailor972 <javajeff972@gmail.com>
2015-07-04 12:31:17 -0500

    Adjust new list view to do model validation

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import resolve
 from django.template.loader import render_to_string
+from django.utils.html import escape
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
@@ -7,7 +8,6 @@ from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
-
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -20,7 +20,6 @@ class HomePageTest(TestCase):
 
 
 class ListViewTest(TestCase):
-
     def test_uses_list_template(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/%d/' % (list_.id,))
@@ -43,14 +42,13 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'other list item 2')
 
     def test_passes_correct_list_to_template(self):
-        other_list = List.objects.create()
+        # other_list = List.objects.create()
         correct_list = List.objects.create()
         response = self.client.get('/lists/%d/' % (correct_list.id,))
         self.assertEqual(response.context['list'], correct_list)
 
 
 class NewListTest(TestCase):
-
     def test_saving_a_POST_request(self):
         self.client.post('/lists/new', data={'item_text': 'A new list item'})
         self.assertEqual(Item.objects.count(), 1)
@@ -65,11 +63,22 @@ class NewListTest(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
 
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
+    def test_invalid_list_items_are_not_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
+
 
 class NewItemTest(TestCase):
-
     def test_can_save_a_POST_request_to_an_existing_list(self):
-        other_list = List.objects.create()
+        # other_list = List.objects.create()
         correct_list = List.objects.create()
 
         self.client.post(
