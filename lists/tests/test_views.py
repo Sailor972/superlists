adc59866e01d58b1ecdc41b48fddc2711921314f
Sailor972 <javajeff972@gmail.com>
2015-07-04 14:09:39 -0500

    Enforced model validation in list view

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -26,7 +26,6 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_passes_correct_list_to_template(self):
-        # other_list = List.objects.create()
         correct_list = List.objects.create()
         response = self.client.get('/lists/%d/' % (correct_list.id,))
         self.assertEqual(response.context['list'], correct_list)
@@ -48,7 +47,6 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'other list item 2')
 
     def test_can_save_a_POST_request_to_an_existing_list(self):
-        # other_list = List.objects.create()
         correct_list = List.objects.create()
 
         self.client.post(
@@ -63,7 +61,6 @@ class ListViewTest(TestCase):
         self.assertEqual(new_item.list, correct_list)
 
     def test_POST_redirects_to_list_view(self):
-        # other_list = List.objects.create()
         correct_list = List.objects.create()
 
         response = self.client.post(
@@ -71,6 +68,14 @@ class ListViewTest(TestCase):
             data={'item_text': 'A new item for an existing list'}
         )
         self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+
+    def test_validation_errors_end_up_on_lists_page(self):
+        list_ = List.objects.create()
+        response = self.client.post('/lists/%d/' % (list_.id,), data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
 
 
 class NewListTest(TestCase):
